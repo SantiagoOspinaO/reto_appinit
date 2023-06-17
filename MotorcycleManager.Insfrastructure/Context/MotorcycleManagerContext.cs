bda16ab64a171a0e31e21869e3ba5ddc35dd293e
SantiagoOspinaO <santiago.m200@outlook.es>
2023-06-17 18:28:09 -0500

    Se crea la clase MotorcycleRepository en la capa de infraestructura

---
+++ MotorcycleManager.Insfrastructure/Context/MotorcycleManagerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MotorcycleManager.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MotorcycleManager.Insfrastructure.Context
+{
+    internal class MotorcycleManagerContext : DbContext
+    {
+        public MotorcycleManagerContext(DbContextOptions<MotorcycleManagerContext> options) : base(options) { }
+        public DbSet<MotorcycleEntity> Motorcycles { get; set; }
+    }
+}
---
+++ MotorcycleManager.Insfrastructure/Repository/MotorcycleRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MotorcycleManager.Domain.Abstraction;
+using MotorcycleManager.Insfrastructure.Context;
+
+namespace MotorcycleManager.Insfrastructure.Repository
+{
+    internal class MotorcycleRepository : IMotorcycleRepository
+    {
+        private readonly MotorcycleManagerContext context;
+        private readonly ILoggerManager LoggerManager;
+    }
+}
